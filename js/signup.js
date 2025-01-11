1d6cf6b7cfad8820d13838bd3a3d08c1bc55e81f
Giyoung Lim <gylim7826@naver.com>
2025-01-11 19:38:17 +0900

    'signupskill'

--- js/signup.js
+++ js/signup.js
@@ -1,5 +1,18 @@
 const completeSignupBtn = document.querySelector(".complete");
 
+document.getElementById("signupForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (username && password && password.length >= 7) {
+    localStorage.setItem(username, password);
+    alert("회원가입 성공~");
+  } else {
+    alert("회원 가입 실패~");
+  }
+});
+
 completeSignupBtn.addEventListener("click", () => {
   window.location.href = "index.html";
 });
